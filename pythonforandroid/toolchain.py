0a4e645a926dd825687977e4be024f975e0624d0
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-08 12:16:09 +0800

    fix memory out

--- pythonforandroid/toolchain.py
+++ pythonforandroid/toolchain.py
@@ -1016,6 +1016,7 @@ class ToolchainCL:
 
             env["ANDROID_NDK_HOME"] = self.ctx.ndk_dir
             env["ANDROID_HOME"] = self.ctx.sdk_dir
+            env['GRADLE_OPTS'] = "-Xmx16g -XX:MaxMetaspaceSize=16g"
 
             gradlew = sh.Command('./gradlew')
 
@@ -1045,8 +1046,13 @@ class ToolchainCL:
 
             # WARNING: We should make sure to clean the build directory before building.
             # See PR: kivy/python-for-android#2705
-            output = shprint(gradlew, "clean", gradle_task, _tail=20,
-                             _critical=True, _env=env)
+            jvm_args = "-Xmx16g -XX:MaxMetaspaceSize=16g"
+            clean_output = shprint(gradlew, "clean", _tail=20, _critical=True, _env=env)
+            gradle_command = [
+                gradle_task,
+                f"-Dorg.gradle.jvmargs={jvm_args}"
+            ]
+            output = shprint(gradlew, *gradle_command, _tail=20, _critical=True, _env=env)
         return output, build_args
 
     def _finish_package(self, args, output, build_args, package_type, output_dir):
